e16b0198d52ed211a7af3891952774a131445070
paul0warren <paul0warren3@gmail.com>
2017-11-13 15:58:15 -0500

    Added real people thumbnails, started thinking about isotope

--- scripts/people.js
+++ scripts/people.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-      $('#tiles .thumbnail').click(function(){
-        $(".thumbnail .details").addClass("hidden");  
-        $(this).find('.details').toggleClass('hidden');
-      });
+       //$('#tiles .thumbnail').click(function(){
+       //  $(".thumbnail .details").addClass("hidden");  
+       //  $(this).find('.details').toggleClass('hidden');
+       //});
+
+      //var $container = $("#tiles");
+
+      //$container.isotope({
+      //  itemSelector: '.thumbnail',
+      //  masonry: {
+      //    columnWidth: 175,
+      //    isFitWidth: true
+      //  }
+      //});
+
+      //$("#tiles .thumbnail").click(function () {
+      //  var $this = $(this),
+      //    tileStyle = $this.hasClass('big') ? {width: 175} : { width: 300};
+      //    $this.toggleClass("big");
+      //    $this.stop().animate(tileStyle);
+
+      //    $container.isotope("reLayout");
+      //});
+
 });
