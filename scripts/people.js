5e9301ef0a4cf964bd79895f32f5e6af3d2e54e5
paul0warren <paul0warren3@gmail.com>
2017-12-01 18:02:03 -0500

    Moved team data in to JSON for ajax loading

--- scripts/people.js
+++ scripts/people.js
@@ -1,30 +1,63 @@
-$(document).ready(function() {
-     /*var sh = function(evt) {
-            var $this = $(this);
-            if (!$this.attr("oWidth")) $this.attr("oWidth", $this.width());
-            if (!$this.attr("oHeight")) $this.attr("oHeight", $this.height());
-            $this.toggleClass("selected");
+$(document).ready(function () {
 
-            if ($this.hasClass("selected")) {
-                 $this.stop().animate({width: 400, height: 400}, function() {
-                  $this.find(".ppl-text").fadeIn(1000);
-                 });
-            } else {
-                 $this.find(".ppl-text").fadeOut(1000, function() {
-                  $this.stop().animate({width: parseInt($this.attr("oWidth")) + 24, height: parseInt($this.attr("oHeight")) + 24})
-                 }); 
-            }
-       };
-       $('#tiles .thumbnail').click(sh).blur(sh);
-       */
-      $detail_section = $("#about-details");
-      $tiles = $(".thumbnail");
-      $tiles.click(function (evt) {
-           var $this = $(this);
-          $tiles.removeClass("selected");
-          $this.addClass("selected");
-          $detail_section.find(".ppl-name").html($this.find(".name").html());
-          $detail_section.find(".ppl-content").html($this.find(".ppl-text").html());
-      });
+    function createPersonSectionHTML(p) {
+        var arr = [];
+
+        var name = p.first_name + " " + p.last_name;
+        arr.push("<div num=\"" + p.id + "\" class=\"thumbnail\" tabindex=\"0\">");
+        arr.push("<div class=\"item-content\">");
+        arr.push("<div class=\"pic\">");
+        arr.push("<img src=\"./images/" + p.image + "\" alt=\"" + p.first_name + "\" />");
+        arr.push("</div>");
+        arr.push("<span class=\"name\">" + name + "</span>");
+        arr.push("<div class=\"details\"><b>" + p.title + "</b></div>");
+        arr.push("<div class=\"ppl-text hidden\">");
+        arr.push("about");
+        if (p.resume) {
+            arr.push("<div class=\"resume\"><a href=\"resumes/ " + p.resume + "\">Resume</a></div>");
+        }
+        arr.push("</div>");
+        arr.push("</div>");
+        arr.push("</div>");
+
+        return arr.join("");
+    };
+
+    function createPeopleSectionHTML(ppl) {
+        var arr = [];
+
+        for (var i = 0; i < ppl.length; i++) {
+            arr.push(createPersonSectionHTML(ppl[i]));
+        }
+
+        return arr.join("");
+    };
+
+    function postLoad() {
+        $detail_section = $("#about-details");
+        $tiles = $(".thumbnail");
+        $tiles.click(function (evt) {
+            var $this = $(this);
+            $tiles.removeClass("selected");
+            $this.addClass("selected");
+            $detail_section.find(".ppl-name").html($this.find(".name").html());
+            $detail_section.find(".ppl-content").html($this.find(".ppl-text").html());
+        });
+    };
+
+    $.ajax({
+        dataType: "json",
+        url: "data/people.json",
+        crossDomain: true,
+        success: function (d) {
+            var peopleContainer = $("#people");
+            var html = "";
+
+            if ((d) && (d.people) & (d.people.length)) html = createPeopleSectionHTML(d.people);
+            
+            peopleContainer.html(html);
+            postLoad();
+        }
+    });
 
 });
